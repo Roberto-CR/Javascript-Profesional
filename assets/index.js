e87a0c0fd038a1e87c46a66d2ce72a9c7e7ecaa1
Roberto <roberto.vargas.0895@gmail.com>
2021-06-08 16:53:20 -0600

    Primer commit

---
+++ assets/index.js
@@ -0,0 +1,17 @@
+import MediaPlayer from "./MediaPlayer.js";
+import AutoPlay from "./plugins/AutoPlay.js";
+
+
+const video = document.querySelector("video");
+const button = document.querySelector("#playPause");
+const muteUnmute = document.querySelector('#unmuteMute')
+
+
+const player = new MediaPlayer({
+    el: video,
+    plugins: [new AutoPlay()]
+  });
+
+
+button.onclick = () => player.togglePlay();
+muteUnmute.onclick = () => player.unmuteMute();
